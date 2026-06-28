257904825d28ae6b56c161167998b48646a49f32
alex <alexaugusthy97@gmail.com>
2026-06-28 11:16:51 +0530

    Fix CORS

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://stay-ease-hotel-booking.onrender.com",
+  baseURL: "https://stay-ease-hotel-booking.onrender.com/api",
 });
 
 export default api;
