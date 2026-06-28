3cab120fced8c0e21cbdceff598a72ecea0167c4
alex <alexaugusthy97@gmail.com>
2026-06-28 11:57:16 +0530

    Test CORS

--- server/server.js
+++ server/server.js
@@ -23,10 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://stay-ease-hotel-booking.vercel.app",
-    ],
+    origin: true,
     credentials: true,
   })
 );
