ccd203e537678e55fb7a1d95d45487b52f3cc21d
alex <alexaugusthy97@gmail.com>
2026-06-29 14:51:18 +0530

    add

--- server/config/mail.js
+++ server/config/mail.js
@@ -5,9 +5,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4,          // <-- Force IPv4
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
