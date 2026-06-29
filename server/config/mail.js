68448be876781036ce5ae57e57d125aa6ed08883
alex <alexaugusthy97@gmail.com>
2026-06-29 11:26:34 +0530

    Configured Brevo SMTP

--- server/config/mail.js
+++ server/config/mail.js
@@ -4,22 +4,13 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  family: 4,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
-});
-
-transporter.verify((err, success) => {
-  if (err) {
-    console.log("SMTP ERROR:", err);
-  } else {
-    console.log("SMTP READY");
-  }
 });
 
 export default transporter;
