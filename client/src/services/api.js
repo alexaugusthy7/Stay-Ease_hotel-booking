6d31863cea5fa6ab7b4488f9446a38f16f55f986
alex <alexaugusthy97@gmail.com>
2026-06-29 11:47:40 +0530

    add changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,17 @@
+// import axios from "axios";
+
+// export const BASE_URL =
+//   "https://stay-ease-hotel-booking.onrender.com";
+
+// const api = axios.create({
+//   baseURL: `${BASE_URL}/api`,
+// });
+
+// export default api;
 import axios from "axios";
 
-export const BASE_URL =
-  "https://stay-ease-hotel-booking.onrender.com";
-
 const api = axios.create({
-  baseURL: `${BASE_URL}/api`,
+  baseURL: "http://localhost:5000/api",
 });
 
 export default api;
