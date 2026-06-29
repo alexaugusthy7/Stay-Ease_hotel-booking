73d19b0fdc90ab350295eb5ee3356b487757dce4
alex <alexaugusthy97@gmail.com>
2026-06-29 10:34:08 +0530

    Force Gmail SMTP to use IPv4

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,19 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
 });
 
-transporter.verify((err) => {
+transporter.verify((err, success) => {
   if (err) {
     console.log("SMTP ERROR:", err);
   } else {
